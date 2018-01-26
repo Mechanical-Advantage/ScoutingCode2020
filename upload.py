06d2de7f042d33466682ee89311df137df5d9d32
Unknown <jaw9912345678910@gmail.com>
2018-01-26 18:45:11 -0500

    Fix formatting
    PrettyPrint enabled

--- upload.py
+++ upload.py
@@ -6,16 +6,22 @@ import serverinfo
 n = 1
 print("Attemting to upload matches...")
 if os.path.isfile('queue.txt'):
-	with open("queue.txt", "r") as file:
-		try:
-			for line in file:
-				requests.post(serverinfo.server + "/submit", data={'event':server.CURRENT_EVENT, 'data': line, 'auth': serverinfo.AUTH})
-				print("Uploaded entry number " + str(n))			
-				n += 1
-		except:
-			print("Failed miserably. Are you connected to the internet?")
-	os.remove('queue.txt')
+    with open("queue.txt", "r") as file:
+        try:
+            for line in file:
+                requests.post(
+                    serverinfo.server + "/submit",
+                    data={
+                        'event': server.CURRENT_EVENT,
+                        'data': line,
+                        'auth': serverinfo.AUTH
+                    })
+                print("Uploaded entry number " + str(n))
+                n += 1
+        except:
+            print("Failed miserably. Are you connected to the internet?")
+    os.remove('queue.txt')
 else:
-	print("The match queue doesn't exist.")
+    print("The match queue doesn't exist.")
 print("Finished.")
 input("Press Enter to exit")

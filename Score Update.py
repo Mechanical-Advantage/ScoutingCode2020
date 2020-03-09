7feb654166898108d5b301361accb464d7c62efb
Scout <6328scout1@gmail.com>
2020-03-09 18:28:38 -0400

    Reformat score update

--- Score Update.py
+++ Score Update.py
@@ -1,11 +1,13 @@
 import tbapy
 import sqlite3 as sql
-conn = sql.connect("c:\\macout\\advantagecout\\data_2020.db")
+conn = sql.connect("c:\\mascout\\advantagescout\\data_2020.db")
 conn1 = sql.connect("c:\\mascout\\advantagescout\\global.db")
 cur = conn.cursor()
 cur1 = conn1.cursor()
-tba = tbapy.TBA('aYSnteXRFd60FJ6KkRgbeZbPNf7z5BWJbcizc9lpHHDcB9lDQYWNhgyMDHTzc5kW')
-event = cur1.execute("SELECT value FROM config WHERE key = event").fetchall()[0][0]
+tba = tbapy.TBA(
+    'aYSnteXRFd60FJ6KkRgbeZbPNf7z5BWJbcizc9lpHHDcB9lDQYWNhgyMDHTzc5kW')
+event = cur1.execute(
+    "SELECT value FROM config WHERE key = event").fetchall()[0][0]
 #event = "2020ctnct"
 data = tba.event_matches(event)
 for x in data:
@@ -14,10 +16,10 @@ for x in data:
         red_score = int(x["alliances"]["red"]["score"])
         comp_level = (x["comp_level"])
         match_number = int(x["match_number"])
-        cur.execute("UPDATE match SET points = ? WHERE event = ? AND match = ? AND alliancecolor = ?", (blue_score, event, match_number, 1))
-        cur.execute("UPDATE match SET points = ? WHERE event = ? AND match = ? AND alliancecolor = ?", (red_score, event, match_number, 0))
-
-
+        cur.execute("UPDATE match SET points = ? WHERE event = ? AND match = ? AND alliancecolor = ?",
+                    (blue_score, event, match_number, 1))
+        cur.execute("UPDATE match SET points = ? WHERE event = ? AND match = ? AND alliancecolor = ?",
+                    (red_score, event, match_number, 0))
 
 
 conn.commit()
